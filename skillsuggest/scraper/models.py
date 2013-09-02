e35035d61e648a762535a6b7feb68fdb40ab69fb
Rudy Dai <rudydai@gmail.com>
2013-09-01 20:54:25 -0700

    save url skill lists in db

--- skillsuggest/scraper/models.py
+++ skillsuggest/scraper/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class publicSkills(models.Model):
+    puburl = models.URLField()
+    skills = models.CharField(max_length=300)
+
+    def __unicode__(self):
+        return self.name
 # Create your models here.
